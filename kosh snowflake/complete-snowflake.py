de5b09706ea1077f7d138606a9d3d7946a968a6d
Aryan Mahesh <aryan.mahesh2010@gmail.com>
2021-04-04 22:12:40 +0530

    *

--- kosh snowflake/complete-snowflake.py
+++ kosh snowflake/complete-snowflake.py
@@ -17,7 +17,7 @@ def drawSnowflake(A, B):
     x1, y1 = A
     x2, y2 = B
     m = (math.sqrt((math.pow((x2-x1), 2))+math.pow((y2-y1), 2)))
-    if m <= 1:
+    if m <= 4:
         drawLine(A, B)
     else:
         p1 = ((x2+(2*x1))/3), ((y2+(2*y1))/3)
@@ -34,10 +34,23 @@ def drawSnowflake(A, B):
         drawSnowflake(p2, B)
         drawSnowflake(tip, p2)
 
+def hexagon(r):
+    a = math.pi/3
+    points = [(0, r), (-r*math.cos(a/2), r*math.sin(a/2)), (-r*math.cos(a/2), -r*math.sin(a/2)),
+                (0, -r), (r*math.cos(a/2), -r*math.sin(a/2)), (r*math.cos(a/2), r*math.sin(a/2))]
+    return points
 
 def main():
-    
-    drawSnowflake((-200, -200), (200, 200))
+    n = int(input())
+    points = hexagon(n)
+    #points = points[::-1]
+    drawSnowflake(points[1], points[0])
+    drawSnowflake(points[2], points[1])
+    drawSnowflake(points[3], points[2])
+    drawSnowflake(points[4], points[3])
+    drawSnowflake(points[5], points[4])
+    drawSnowflake(points[0], points[5])
+
     turtle.done()
 
 if __name__ == '__main__':
